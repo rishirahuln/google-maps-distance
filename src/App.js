c32cdda21becc5e8e6b3bfe6ce2853207bea7dc0
rishirahuln <nrishirahul@gmail.com>
2023-01-20 00:14:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { Box, Button, ButtonGroup, Flex, HStack, Input, SkeletonText, Text } from "@chakra-ui/react";
+import { useJsApiLoader, GoogleMap, Autocomplete, DirectionsRenderer, MarkerF } from "@react-google-maps/api";
+import { useRef, useState } from "react";
+
+const center = { lat: 23.2599, lng: 77.4126 };
+
+function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
+
+  const [map, setMap] = useState(null);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  const originRef = useRef();
+  const destinationRef = useRef();
+
+  if (!isLoaded) {
+    return <SkeletonText />;
+  }
+
+  async function calculateData() {
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+
+  function clearData() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
+
+  return (
+    <Flex position="relative" flexDirection="column" alignItems="center" h="100vh" w="100vw">
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
+        <GoogleMap center={center} zoom={5} mapContainerStyle={{ height: "100%", width: "100%" }} onLoad={(map) => setMap(map)}>
+          <MarkerF position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+      <Box minW="container.md" zIndex="1" bg="white" boxShadow="md" rounded="lg" mt="20px" p="20px">
+        <HStack spacing="10px" justifyContent="space-between">
+          <Autocomplete>
+            <Input type="text" placeholder="Origin" ref={originRef} />
+          </Autocomplete>
+
+          <Autocomplete>
+            <Input type="text" placeholder="Destination" ref={destinationRef} />
+          </Autocomplete>
+
+          <ButtonGroup>
+            <Button type="submit" colorScheme="blue" onClick={calculateData}>Calculate</Button>
+            <Button type="button" colorScheme="red" onClick={clearData}>Clear</Button>
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing="10px" justifyContent="space-between" mt="10px">
+          <Text>Distance: {distance}</Text>
+          <Text>Duration: {duration}</Text>
+          <Button type="button" colorScheme="yellow" onClick={() => map.panTo(center)}>Re-center</Button>
+        </HStack>
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
